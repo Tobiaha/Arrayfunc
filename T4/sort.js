07a694e09ba6081ad3d6349d04e3bc4ba26998ba
tobiaha <topihammarberg28@gmail.com>
2024-09-11 18:09:05 +0300

    sort t4 ja t5

--- T4/sort.js
+++ T4/sort.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const numbers = [5, 1, 45, 13, 12, 7];
 
-function sortArray() {
-    console.log(`original array: ${numbers}`);
-    numbers.sort((a, b) => a - b);
-
-    console.log(`ascending array: ${numbers}`);
+function sortArray(numbers, order) {
+    if (order === "asc") {
+        numbers.sort((a, b) => a - b);//ascending
+    } else if (order === "desc") {
+        numbers.sort((a, b) => b - a); //descending
+    } else {
+        console.log('Invalid function parameter')
+        return;
+    }
+    return numbers;
+    //T4 ja T5
 }
-sortArray();
+
+const numbers = [5, 1, 45, 13, 12, 7];
+console.log(`original array: ${numbers}`);
+
+console.log(sortArray([...numbers], "asc")); //ascending
+console.log(sortArray([...numbers], "desc")); //descending
